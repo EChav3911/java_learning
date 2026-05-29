7a0999a1ea513fa3a5edaea64090fe8203eed3b7
Edgar Chavez <echav3911@gmail.com>
2026-05-29 10:00:14 -0700

    Starting SecondsAndMinutesChallenge.java

---
+++ src/SecondsAndMinutesChallenge.java
@@ -0,0 +1,14 @@
+public class SecondsAndMinutesChallenge {
+
+    static void main() {
+
+    }
+
+    public static void getDurationString(int seconds){
+
+    }
+
+    public static void getDurationString(int minutes, int seconds){
+
+    }
+}
