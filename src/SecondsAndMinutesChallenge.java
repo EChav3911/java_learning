a0af90a7803ecc224addaf28fc34b93d5fbd02e7
Edgar Chavez <echav3911@gmail.com>
2026-05-29 10:33:03 -0700

    Created two methods, one to convert minutes and seconds to total seconds, the next to take the total seconds and divide into hours, minutes, and seconds.

--- src/SecondsAndMinutesChallenge.java
+++ src/SecondsAndMinutesChallenge.java
@@ -2,13 +2,37 @@ public class SecondsAndMinutesChallenge {
 
     static void main() {
 
+        int minutes = 190;
+        int seconds = 44;
+
+        //getDurationString(minutes, seconds);
+        getDurationString(getDurationString(minutes, seconds));
     }
 
     public static void getDurationString(int seconds){
 
+        int minutes = seconds/60;
+        int hours = minutes/60;
+        seconds = seconds - (minutes * 60);
+        minutes = minutes - (hours * 60);
+
+        System.out.println(hours + "h " + minutes + "m " + seconds + "s");
     }
 
-    public static void getDurationString(int minutes, int seconds){
+    public static int getDurationString(int minutes, int seconds){
 
+        int totalSeconds = -1;
+
+        if (minutes >= 0 && (seconds >= 0 && seconds <= 59)) {
+
+            int minutesToSeconds = minutes * 60;
+            totalSeconds = minutesToSeconds + seconds;
+
+            System.out.println(0+"h " + 0+"m " + totalSeconds+"s");
+        }else {
+            System.out.println("invalid entry");
+        }
+
+        return totalSeconds;
     }
 }
