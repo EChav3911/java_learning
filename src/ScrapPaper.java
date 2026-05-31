bc574d86707cb59972cfc2c15645ecec50a596a9
Edgar Chavez <echav3911@gmail.com>
2026-05-31 09:23:15 -0700

    Started ScrapPaper class for practicing Java

---
+++ src/ScrapPaper.java
@@ -0,0 +1,25 @@
+public class ScrapPaper {
+
+    static void main() {
+        printYearsAndDays(525600);
+    }
+
+    public static void printYearsAndDays (long minutes) {
+
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+
+            return;
+        }
+
+        long hours = minutes / 60;
+        long days = hours / 24;
+        long remainingDays = hours % 24;
+        long years = days / 365;
+
+        System.out.println (minutes + " min = " + years + " y and " + remainingDays + " d");
+
+    }
+
+
+}
